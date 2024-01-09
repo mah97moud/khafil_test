201acf536261b986a316f7746eee383b9849bf7d
Mahmoud Atef <mahmoud97atef@gmail.com>
2024-01-09 11:59:39 +0200

    make user type dynamic

--- lib/features/register/ui/widgets/register_user_type.dart
+++ lib/features/register/ui/widgets/register_user_type.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:khafil_test/features/register/ui/widgets/register_form_field.dart';
+import 'package:khafil_test/core/app/di.dart';
 import 'package:khafil_test/features/register/ui/widgets/register_form_text.dart';
+
+import '../../../../core/managers/colors_manager.dart';
 
 class RegisterUserType extends StatelessWidget {
   const RegisterUserType({
@@ -9,16 +11,39 @@ class RegisterUserType extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        RegisterFormText(
+        const RegisterFormText(
           'User Type',
         ),
-        RegisterFormField(
-          suffixIcon: Icon(
-            Icons.keyboard_arrow_down,
-            size: 30.0,
+        Container(
+          margin: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
+          decoration: ShapeDecoration(
+            color: ColorsManager.grey50,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16),
+            ),
+          ),
+          height: 56,
+          child: DropdownButtonFormField<int>(
+            decoration: const InputDecoration(
+              fillColor: ColorsManager.grey50,
+              contentPadding: EdgeInsets.symmetric(
+                horizontal: 16.0,
+                vertical: 16.0,
+              ),
+            ),
+            icon: const Icon(Icons.keyboard_arrow_down_rounded),
+            items: types
+                .map(
+                  (e) => DropdownMenuItem<int>(
+                    value: e.value,
+                    child: Text(e.label),
+                  ),
+                )
+                .toList(),
+            onChanged: (value) {},
           ),
         )
       ],
